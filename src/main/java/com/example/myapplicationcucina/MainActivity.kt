94ec98e74030bc0c5e3b5273ff15346adebfc0d9
alberto rossi <albertorossi@etik.com>
2025-02-22 15:46:18 +0100

    Aggiunto il progetto iniziale

---
+++ src/main/java/com/example/myapplicationcucina/MainActivity.kt
@@ -0,0 +1,51 @@
+non mi stufero mai di chiederti chi seipackage com.example.myapplicationcucina
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplicationcucina.ui.theme.MyApplicationCucinaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationCucinaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationCucinaTheme {
+        Greeting("Android")
+    }
+}bash
+npx react-native init CucinaApp
+bash
+npx react-native init CucinaApp
+        
